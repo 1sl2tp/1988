35a660f4ac34a5d04e5263992f6e527682bce03e
1sl2tp <1sl2tp@gmail.com>
2026-09-23 04:04:12 +0700

    Refresh PWA cache shell

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="1988-shell-v8";
-const SHELL=["./","./index.html","./src/style.css?v=1988-8","./src/api.js?v=1988-8","./src/app.js?v=1988-8","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
+const CACHE="1988-shell-v9";
+const SHELL=["./","./index.html","./src/style.css?v=1988-9","./src/api.js?v=1988-9","./src/app.js?v=1988-9","./manifest.webmanifest","./icons/icon-180.png","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));
   self.skipWaiting();
@@ -14,8 +14,18 @@ self.addEventListener("fetch",event=>{
   const url=new URL(req.url);
   if(url.origin!==location.origin)return;
   if(req.mode==="navigate"){
-    event.respondWith(fetch(req).then(res=>{const clone=res.clone();caches.open(CACHE).then(c=>c.put("./index.html",clone));return res;}).catch(()=>caches.match("./index.html")));
+    event.respondWith(fetch(req).then(res=>{
+      const clone=res.clone();
+      caches.open(CACHE).then(c=>c.put("./index.html",clone));
+      return res;
+    }).catch(()=>caches.match("./index.html")));
     return;
   }
-  event.respondWith(caches.match(req).then(hit=>hit||fetch(req).then(res=>{if(res.ok){const clone=res.clone();caches.open(CACHE).then(c=>c.put(req,clone));}return res;})));
+  event.respondWith(caches.match(req).then(hit=>hit||fetch(req).then(res=>{
+    if(res.ok){
+      const clone=res.clone();
+      caches.open(CACHE).then(c=>c.put(req,clone));
+    }
+    return res;
+  })));
 });
