7ff1b4bdb806d2ccc1c2c73f37f7b6ad7f531d49
1sl2tp <1sl2tp@gmail.com>
2026-09-23 03:51:37 +0700

    Add DeArrow branding API

--- src/api.js
+++ src/api.js
@@ -26,6 +26,7 @@ const playlistNext=(id,nextpage)=>call("playlist_next",{id,nextpage});
 const channel=(id)=>call("channel",{id});
 const channelNext=(id,nextpage)=>call("channel_next",{id,nextpage});
 const sponsors=(id)=>call("sponsors",{id});
+const branding=(ids)=>call("branding",{ids:(Array.isArray(ids)?ids:[]).join(",")});
 function playerUrl(id){
   const url=new URL("https://www.youtube-nocookie.com/embed/"+encodeURIComponent(id));
   url.searchParams.set("autoplay","1");
@@ -48,4 +49,4 @@ function playlistPlayerUrl(id){
   url.searchParams.set("origin",location.origin);
   return url.toString();
 }
-window.YT1988_API={trending,search,searchNext,suggestions,video,playlist,playlistNext,channel,channelNext,sponsors,playerUrl,playlistPlayerUrl};
+window.YT1988_API={trending,search,searchNext,suggestions,video,playlist,playlistNext,channel,channelNext,sponsors,branding,playerUrl,playlistPlayerUrl};
