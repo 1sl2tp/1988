63898b68a388f3103b55c50ef15bb8b608472f4f
1sl2tp <1sl2tp@gmail.com>
2026-09-25 02:00:20 +0700

    Safari fullscreen: refresh service worker cache

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const CACHE='1988-main-semantic-context-v129';
+const CACHE='1988-main-safari-fullscreen-v130';
 const SHELL=[
   './',
   './index.html',
   './manifest.webmanifest',
   './silent.wav',
-  './src/style.css?v=context-brief-129',
-  './src/app.js?v=semantic-context-129',
+  './src/style.css?v=safari-fullscreen-130',
+  './src/app.js?v=safari-fullscreen-130',
   './src/media-core.js?v=2',
   './src/html5-background.js?v=9',
   './src/channel-library.js?v=sources-88',
