f00fd6be735995ded0970a7c67f12be67f4f42ef
1sl2tp <1sl2tp@gmail.com>
2026-09-24 17:27:19 +0700

    main: cache channel library build

--- sw.js
+++ sw.js
@@ -1,16 +1,17 @@
 'use strict';
 
-const CACHE='1988-main-broader-feed-v59';
+const CACHE='1988-main-channel-library-v60';
 const SHELL=[
   './',
   './index.html',
   './manifest.webmanifest',
   './silent.wav',
-  './src/style.css?v=regional-feed-45',
-  './src/app.js?v=broader-feed-59',
+  './src/style.css?v=channel-library-60',
+  './src/app.js?v=channel-library-60',
   './src/media-core.js?v=2',
   './src/html5-background.js?v=9',
-  './src/yt-local.js?v=hype-feed-53',
+  './src/channel-library.js?v=channel-library-60',
+  './src/yt-local.js?v=channel-library-60',
   './icons/icon-180.png',
   './icons/icon-192.png',
   './icons/icon-512.png'
