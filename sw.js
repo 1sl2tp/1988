075be5374535f9103dddbeed33c59702a3bfb759
1sl2tp <1sl2tp@gmail.com>
2026-09-24 13:40:59 +0700

    main: refresh PWA cache for iframe native PiP

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const CACHE='1988-main-minimal-v26';
+const CACHE='1988-main-iframe-pip-v27';
 const SHELL=[
   './',
   './index.html',
   './manifest.webmanifest',
   './silent.wav',
-  './src/style.css?v=minimal-26',
-  './src/app.js?v=minimal-26',
+  './src/style.css?v=iframe-pip-27',
+  './src/app.js?v=iframe-pip-27',
   './src/media-core.js?v=2',
   './src/html5-background.js?v=9',
   './src/yt-local.js?v=4',
