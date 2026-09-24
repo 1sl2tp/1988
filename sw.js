88426f15488d471dde552485f36dead64a036c20
1sl2tp <1sl2tp@gmail.com>
2026-09-25 06:48:25 +0700

    UI: redesign mobile landscape watch page

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const CACHE='1988-main-watch-browse-v180';
+const CACHE='1988-main-watch-browse-v181';
 const SHELL=[
   './',
   './index.html',
   './manifest.webmanifest',
   './silent.wav',
-  './src/style.css?v=watch-browse-180',
-  './src/app.js?v=watch-browse-180',
+  './src/style.css?v=watch-browse-181',
+  './src/app.js?v=watch-browse-181',
   './src/media-core.js?v=2',
   './src/html5-background.js?v=9',
   './src/channel-library.js?v=sources-88',
