8cb36715d970f76ec63cfbbda22ce2ace881d431
1sl2tp <1sl2tp@gmail.com>
2026-09-24 18:11:26 +0700

    main: refresh PWA cache for source icon header

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const CACHE='1988-main-video-card-v67';
+const CACHE='1988-main-source-icon-v68';
 const SHELL=[
   './',
   './index.html',
   './manifest.webmanifest',
   './silent.wav',
-  './src/style.css?v=video-card-67',
+  './src/style.css?v=source-icon-68',
   './src/app.js?v=source-header-65',
   './src/media-core.js?v=2',
   './src/html5-background.js?v=9',
