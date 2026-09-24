e673e6ac38aa6d93cf895b7b63530749e0f0c5f1
1sl2tp <1sl2tp@gmail.com>
2026-09-25 04:01:29 +0700

    PiP: align controls horizontally beside fullscreen

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const CACHE='1988-main-pip-safe-controls-v156';
+const CACHE='1988-main-pip-inline-controls-v157';
 const SHELL=[
   './',
   './index.html',
   './manifest.webmanifest',
   './silent.wav',
-  './src/style.css?v=pip-safe-controls-156',
+  './src/style.css?v=pip-inline-controls-157',
   './src/app.js?v=pip-safe-controls-156',
   './src/media-core.js?v=2',
   './src/html5-background.js?v=9',
