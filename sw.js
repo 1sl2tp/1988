cab43ed1c7db40da6b1b2a3ba3c45b6ebc49a980
1sl2tp <1sl2tp@gmail.com>
2026-09-25 09:05:22 +0700

    Cache: service worker v210

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const CACHE='1988-main-watch-browse-v209';
+const CACHE='1988-main-watch-browse-v210';
 const SHELL=[
   './',
   './index.html',
   './manifest.webmanifest',
   './silent.wav',
-  './src/style.css?v=watch-browse-209',
-  './src/app.js?v=watch-browse-209',
+  './src/style.css?v=watch-browse-210',
+  './src/app.js?v=watch-browse-210',
   './src/media-core.js?v=2',
   './src/html5-background.js?v=9',
   './src/channel-library.js?v=sources-88',
