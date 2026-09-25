827d8c534f520c85d8591bb35285e04cef49fa53
1sl2tp <1sl2tp@gmail.com>
2026-09-25 15:59:17 +0700

    PWA: service worker v227

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const CACHE='1988-main-watch-browse-v226';
+const CACHE='1988-main-watch-browse-v227';
 const SHELL=[
   './',
   './index.html',
   './manifest.webmanifest',
   './silent.wav',
-  './src/style.css?v=watch-browse-226',
-  './src/app.js?v=watch-browse-226',
+  './src/style.css?v=watch-browse-227',
+  './src/app.js?v=watch-browse-227',
   './src/media-core.js?v=2',
   './src/html5-background.js?v=9',
   './src/channel-library.js?v=sources-88',
